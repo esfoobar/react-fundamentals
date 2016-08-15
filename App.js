a8c57f9940b8adf54e5c3c3efea094ba5cf3c26d
Jorge Escobar <j@jorge3.com>
2016-08-15 19:31:18 -0400

    initial version of React App

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+
+// state component
 class App extends React.Component {
     render(){
-        return <div>Hello</div>
+        return <h1>Hello World!</h1>
     }
 }
 
-export default App
+const App2 = () => <h1>Hello Guys</h1> //stateless component
+
+// to put multiple elements, I wrap in parenthesis
+class App3 extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <b>Bold</b>
+            </div>
+        )
+    }
+}
+export default App3
