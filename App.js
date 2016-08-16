74dc48dd325b43a46070a99f94d83201edaf506a
Jorge Escobar <j@jorge3.com>
2016-08-15 20:02:55 -0400

    owner/ownee

--- App.js
+++ App.js
@@ -4,10 +4,8 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(){
         super();
-        this.state = {
-            txt: 'this is the state txt',
-            cat: 0
-         }
+        this.state = { txt: '' }
+        this.update = this.update.bind(this)
     }
     update(e){
         this.setState({ txt: e.target.value })
@@ -15,12 +13,22 @@ class App extends React.Component {
     render(){
         return (
             <div>
-            <input type="text"
-                onChange={this.update.bind(this)} />
-            <h1>{this.state.txt}</h1>
+                <Widget txt={this.state.txt} update={this.update} />
+                <Widget txt={this.state.txt} update={this.update} />
+                <Widget txt={this.state.txt} update={this.update} />
             </div>
         );
     }
+}
+
+const Widget = (props) => {
+    return (
+        <div>
+        <input type="text"
+            onChange={props.update} />
+        <h1>{props.txt}</h1>
+        </div>
+    );
 }
 
 ReactDOM.render(
